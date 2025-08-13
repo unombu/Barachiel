484d05cc43d36c9f3a3e67065216cd7b6ee06cf7
unombu <belen.ungari@gmail.com>
2025-08-13 10:38:05 -0300

    showoptions in handleopselect

--- thinking.js
+++ thinking.js
@@ -127,6 +127,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       processResponse(nextResponseId);
       if (data.responses[nextResponseId].action == "RENOVE_OPTION"){
         currentOptions = currentOptions.filter(option => option.id !== optionData.id);
+        showOptions(options);
       }
     } else {
       // Si una opción no tiene 'next', podría ser el final de esa rama
