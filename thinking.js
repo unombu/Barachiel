ac5b1293c90a67fa735c6b37bfde45430e5cdd6d
unombu <belen.ungari@gmail.com>
2025-08-16 17:00:41 -0300

    stars fix

--- thinking.js
+++ thinking.js
@@ -151,6 +151,7 @@ function handleAction(action, data) {
         }
         break;
       case "STARS":
+        stars();
         break;
       case "ERROR":
         break;
@@ -174,6 +175,7 @@ function stars() {
     return;
   }
   overlay.style.display = "flex";
+  generateStars(169);
   backtocheckpoint();
 }
 
@@ -292,6 +294,39 @@ function killswitch(delay = 2000) {
 // ----------------------------------------------------
 // ## AUXILIARES
 // ----------------------------------------------------
+
+function generateStars(numberOfStars) {
+  const container = document.querySelector('.star-container');
+
+  if (!container) {
+    console.error('El contenedor .star-container no fue encontrado.');
+    return;
+  }
+
+  const animationTypes = ['pulsing', 'flickering', 'rotating'];
+
+  for (let i = 0; i < numberOfStars; i++) {
+    const star = document.createElement('div');
+    star.classList.add('star');
+
+    const animationType = animationTypes[i % animationTypes.length];
+    star.setAttribute('data-star-type', animationType);
+
+    // Posiciona la estrella aleatoriamente
+    const x = Math.random() * 100;
+    const y = Math.random() * 100;
+    star.style.left = `${x}vw`;
+    star.style.top = `${y}vh`;
+
+    // Si la estrella es de tipo "flickering", asigna un retraso aleatorio
+    if (animationType === 'flickering') {
+      const randomDelay = Math.random() * 2; // Retraso aleatorio entre 0 y 2 segundos
+      star.style.animationDelay = `${randomDelay}s`;
+    }
+
+    container.appendChild(star);
+  }
+}
 
 function typeText(text, className = "") {
   return new Promise((resolve) => {
